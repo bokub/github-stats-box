0e6a9a23f14aaccb0be4e4c3e0e6fc068910235d
Boris Kubiak <kubiakboris@gmail.com>
2020-08-20 14:28:51 +0200

    Rollback commit icon

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function updateGist(stats) {
     const gistContent =
         [
             ['⭐', `Total Stars`, humanize(stats.totalStars)],
-            ['✔️', countAllCommits ? 'Total Commits' : 'Past Year Commits', humanize(stats.totalCommits)],
+            ['➕', countAllCommits ? 'Total Commits' : 'Past Year Commits', humanize(stats.totalCommits)],
             ['🔀', `Total PRs`, humanize(stats.totalPRs)],
             ['🚩', `Total Issues`, humanize(stats.totalIssues)],
             ['📦', `Contributed to`, humanize(stats.contributedTo)],
